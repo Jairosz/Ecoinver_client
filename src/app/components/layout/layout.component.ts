6b090d0a5ddc151ebad62946c884d8c1f1329a8a
Ecoinver-Software <software@ecoinver.es>
2025-03-12 16:45:19 +0100

    NavbarFix
    Navbar Fix

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../navbar/navbar.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavbarComponent, SidebarComponent, FooterComponent, RouterOutlet],
+  imports: [ SidebarComponent, FooterComponent, RouterOutlet],
   template: `
-    <app-navbar></app-navbar>
     <div class="flex">
       <app-sidebar></app-sidebar>
       <div class="flex-1 p-4">
