d7a54577b628574391cc5d1694cd81546bf457bb
Melk236 <162185560+Melk236@users.noreply.github.com>
2025-03-20 16:57:17 +0100

    Cambios en el componente comercial
    Hemos creado el componente comercial con una plantilla predefinida de tailwind que es una tabla donde se ven los datos de los comerciales , junto al servicio comercial que hace la conexión con la api

--- src/app/services/comercial-service.service.ts
+++ src/app/services/comercial-service.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../app/environment/environment';
-interface Comercial {
-  codCliente: string;
-  nombCliente: string;
-  fInicio: string;
-  fFin: string;
-  kg: string;
+interface comercial {
+  
+  clientCode: string;
+  clientName: string;
+  startDate: string;
+  endDate: string;
+  kgs: number;
 }
 
 @Injectable({
@@ -18,8 +19,8 @@ export class ComercialServiceService {
 
   constructor(private http:HttpClient) { }
 
-  getComercial ():Observable<Comercial[]>{
-    const url=environment.baseUrl+'/comercial';
-    return this.http.get<Comercial[]>(url);
+  getComercial ():Observable<comercial[]>{
+    const url=environment.baseUrl+'/commercialneeds';
+    return this.http.get<comercial[]>(url);
   }
 }
