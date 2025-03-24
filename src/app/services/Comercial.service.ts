c56c707a7769201b3ba6a9a134539fd9b0bd4b03
Melk236 <162185560+Melk236@users.noreply.github.com>
2025-03-24 16:55:58 +0100

    Crud comercial
    Crear la funcionalidad de los botones crear,borrar y editar y validando los campos desde el front

---
+++ src/app/services/Comercial.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environment/environment';
+import { CreateComercial } from '../types/createComercial';
+
+export interface Comercial {
+  id:number;
+  clientCode: string;
+  clientName: string;
+  startDate: Date;
+  endDate: Date;
+  kgs: number;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ComercialServiceService {
+   private url=environment.baseUrl+'/commercialneeds';
+  constructor(private http:HttpClient) { }
+
+  getComercial ():Observable<Comercial[]>{
+   
+    return this.http.get<Comercial[]>(this.url);
+  }
+  deleteComercial(id:number):Observable<Comercial[]>{
+  ;
+    return this.http.delete<Comercial[]>(this.url+'/'+id);
+  }
+  createComercial(clientData:CreateComercial):Observable<Comercial[]>{
+    return this.http.post<Comercial[]>(this.url,clientData);
+  }
+  
+}
---
+++ src/app/types/createComercial.ts
@@ -0,0 +1,10 @@
+export interface CreateComercial {
+   
+        clientCode:string;
+        clientName:string;
+        startDate:Date | undefined;
+        endDate:  Date | undefined;
+        kgs:number;
+    
+    
+}
