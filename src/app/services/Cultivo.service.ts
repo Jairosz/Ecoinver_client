a912be53161027c2635afc793e95834ad2ed0920
Ecoinver-Software <software@ecoinver.es>
2025-03-25 10:05:58 +0100

    Cultive Get
    Cultive Get

---
+++ src/app/services/Cultivo.service.ts
@@ -0,0 +1,22 @@
+// cultivo.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environment/environment';
+import { Cultive } from '../types/Cultive'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CultivoService {
+  // Combinas la baseUrl con tu endpoint (p.ej. /Cultives, /Cultivos, etc.)
+  private baseUrl = environment.baseUrl + '/Erp/cultives';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Cultive[]> {
+    return this.http.get<Cultive[]>(this.baseUrl);
+  }
+
+  // Podrías agregar más métodos: create, update, delete, etc.
+}
